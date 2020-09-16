2cbcbc3bf329fb2ef7c1cfc56b717e80588be279
Choi JoonMyung <pizard@korea.ac.kr>
2020-09-16 16:39:30 +0900

    test

--- miniimagenet_train.py
+++ miniimagenet_train.py
@@ -55,10 +55,10 @@ def main():
     print('Total trainable tensors:', num)
 
     # batchsz here means total episode number
-    mini = MiniImagenet('./dataset/miniimagenet/', mode='train', n_way=args.n_way, k_shot=args.k_spt,
+    mini = MiniImagenet('./dataset/miniImageNet/', mode='train', n_way=args.n_way, k_shot=args.k_spt,
                         k_query=args.k_qry,
                         batchsz=10000, resize=args.imgsz)
-    mini_test = MiniImagenet('./dataset/miniimagenet', mode='test', n_way=args.n_way, k_shot=args.k_spt,
+    mini_test = MiniImagenet('./dataset/miniImageNet', mode='test', n_way=args.n_way, k_shot=args.k_spt,
                              k_query=args.k_qry,
                              batchsz=100, resize=args.imgsz)
 
